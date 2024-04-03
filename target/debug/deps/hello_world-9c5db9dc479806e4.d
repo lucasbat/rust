df302dc458bb73f53119498c5981517abddd59c3
TR779246 <lucas.batista@squadra.com.br>
2024-04-03 08:30:32 -0300

    first commit

---
+++ target/debug/deps/hello_world-9c5db9dc479806e4.d
@@ -0,0 +1,5 @@
+/home/desenvolvimento/projects/hello-world/target/debug/deps/hello_world-9c5db9dc479806e4: src/main.rs
+
+/home/desenvolvimento/projects/hello-world/target/debug/deps/hello_world-9c5db9dc479806e4.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/hello-world.d
@@ -0,0 +1,1 @@
+/home/desenvolvimento/projects/hello-world/target/debug/hello-world: /home/desenvolvimento/projects/hello-world/src/main.rs
